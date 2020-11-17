a036651cf02afb57e97f6cb15552dfeec862ee23
Ruben Silva <rfc.silva@campus.fct.unl.pt>
2020-11-17 11:19:13 +0000

    Deleted Unused code

--- src/main/scala/Tester.scala
+++ src/main/scala/Tester.scala
@@ -3,5 +3,4 @@ import earthExplorer.EarthExplorerApi
 object Tester {
   def main(args: Array[String]): Unit = {
   }
-//{"username": "rfc.silva", "password": "Rubencristianas2", "authType": "", "catalogId": "EE"}
 }
